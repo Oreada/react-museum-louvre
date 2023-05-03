f41cf418a2147546c4a713268be7f86dd83829a7
Olga Antushevich <antushevich@gmail.com>
2023-05-03 20:26:25 +0300

    feat: add styles

--- src/components/SlideImg/SlideImg.tsx
+++ src/components/SlideImg/SlideImg.tsx
@@ -10,13 +10,15 @@ export interface SlideProps {
 
 export const SlideImg = ({ item }: SlideProps) => {
 	return (
-		<div className={style.wrapper}>
-			<div className={style.text}>
-				<h2 className={style.title}>Welcome to the Louvre</h2>
-				<h3 className={style.subtitle}>From the castle to the museum</h3>
-				<button className={style.button} type="button">Discover the Louvre</button>
+		<div className={style.container}>
+			<div className={style.wrapper}>
+				<div className={style.text}>
+					<h2 className={style.title}>Welcome to&nbsp;the Louvre</h2>
+					<h3 className={style.subtitle}>From the castle to the museum</h3>
+					<button className={style.button} type="button">Discover the Louvre</button>
+				</div>
+				<img className={style.image} src={item.image} alt="Painting from Louvre" />
 			</div>
-			<img className={style.image} src={item.image} alt="Painting from Louvre" />
 		</div>
 	);
 };
