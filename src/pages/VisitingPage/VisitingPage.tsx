250fa5e46178a75e85c0c38993c874a716fdbb60
Olga Antushevich <antushevich@gmail.com>
2023-06-01 19:33:20 +0300

    feat: create VisitingPage

---
+++ src/pages/VisitingPage/VisitingPage.tsx
@@ -0,0 +1,14 @@
+import style from './VisitingPage.module.css';
+
+export const VisitingPage = () => {
+	return (
+		<div className={style.main}>
+			<div className={style.container}>
+				<h2 className={style.title}>Virtual&nbsp;tour</h2>
+				<ul className={style.list}>
+					{ }
+				</ul>
+			</div>
+		</div>
+	)
+};
